c94160a8b0158a0d9b2b3e385529faa2b6c654ca
James Pedid <jamespedid@gmail.com>
2018-05-13 19:37:18 -0400

    added combinedRegistryResolve added combinedRegistryResolve.test updated index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { innerject } from './innerject';
 import { innerjectFunc } from './innerjectFunc';
+import { combinedRegistryResolve } from './combinedRegistryResolve';
 import Registry, { defaultRegistry } from './registry';
 
 export {
     innerject,
     innerjectFunc,
+    combinedRegistryResolve,
     Registry,
     defaultRegistry,
 }
