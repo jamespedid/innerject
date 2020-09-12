acbf053bedcf85a7cf54e6664dc0aec76b958318
James Pedid <jamespedid@gmail.com>
2020-09-12 12:02:40 -0400

    fixed wrapper export

--- esm/wrapper.js
+++ esm/wrapper.js
@@ -1,7 +1,7 @@
-import innerject from '../lib/index.js'
+import Innerject from '../lib/index.js'
 
-export const Registry = innerject.Registry;
-export const innerject = innerject.innerject;
-export const innerjectFunc = innerject.innerjectFunc;
-export const combinedRegistryResolve = innerject.combinedRegistryResolve;
-export const defaultRegistry = innerject.defaultRegistry;
+export const Registry = Innerject.Registry;
+export const innerject = Innerject.innerject;
+export const innerjectFunc = Innerject.innerjectFunc;
+export const combinedRegistryResolve = Innerject.combinedRegistryResolve;
+export const defaultRegistry = Innerject.defaultRegistry;
