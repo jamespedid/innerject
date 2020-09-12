bb1d87cea9ad0f1e39c1d0397a54cfc104d35822
James Pedid <jamespedid@gmail.com>
2020-09-12 11:48:49 -0400

    added esm wrapper

---
+++ esm/wrapper.js
@@ -0,0 +1,7 @@
+import innerject from '../lib/index.js'
+
+export const Registry = innerject.Registry;
+export const innerject = innerject.innerject;
+export const innerjectFunc = innerject.innerjectFunc;
+export const combinedRegistryResolve = innerject.combinedRegistryResolve;
+export const defaultRegistry = innerject.defaultRegistry;
