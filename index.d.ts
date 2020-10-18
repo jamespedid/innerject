f09da962cf33830ae1a6c3d3d08752e962f79aa6
James Pedid <jamespedid@gmail.com>
2020-10-17 21:45:27 -0400

    slightly adjusted type defs

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,27 @@
 declare module "innerject" {
-    type ClassType<T> = { new(...args): T }
-    type KeyType<T> = string | symbol | ClassType<T> | T;
-    type ValueType<T> = T|ClassType<T>|Factory<T>;
-    type Factory<T> = (...args: any[]) => T;
-    type RegistryType = 'class'|'instance'|'factory'|'instanceFactory';
-    type FunctionType<T> = (...args: any[]) => T;
-    type Resolver = (key: string, ...args: any[]) => any;
-    type MapArgsHandler = (resolve: Resolver, ...args: any[]) => any[];
+    export type ClassType<T> = { new(...args): T }
+    export type KeyType<T> = string | symbol | ClassType<T> | T;
+    export type ValueType<T> = T|ClassType<T>|Factory<T>;
+    export type Factory<T> = (...args: any[]) => T;
+    export type RegistryType = 'class'|'instance'|'factory'|'instanceFactory';
+    export type FunctionType<T> = (...args: any[]) => T;
+    export type Resolver = (key: string, ...args: any[]) => any;
+    export type MapArgsHandler = (resolve: Resolver, ...args: any[]) => any[];
 
-    export class Registry {
-        public static defaultRegistry: Registry;
+    export interface IRegistry {
+        has(key: string): boolean;
+        unregister(key: string): void;
+        reset(): void;
+        registerClass<T=unknown>(key: KeyType<T>, Class: ClassType<T>): void;
+        registerInstance<T=unknown>(key: KeyType<T>, instance: T): void;
+        registerFactory<T=unknown>(key: KeyType<T>, factory: Factory<T>): void;
+        registerInstanceFactory<T=unknown>(key: KeyType<T>, factory: Factory<T>): void;
+        register<T=unknown>(type: RegistryType, key: KeyType<T>, object: ValueType<T>);
+        resolve<T=unknown>(key: KeyType<T>, ...args: any[]): T;
+    }
+
+    export class Registry implements IRegistry {
+        static defaultRegistry: IRegistry;
         public has(key: string): boolean;
         public unregister(key: string): void;
         public reset(): void;
@@ -18,7 +30,7 @@ declare module "innerject" {
         public registerFactory<T=unknown>(key: KeyType<T>, factory: Factory<T>): void;
         public registerInstanceFactory<T=unknown>(key: KeyType<T>, factory: Factory<T>): void;
         public register<T=unknown>(type: RegistryType, key: KeyType<T>, object: ValueType<T>);
-        public resolve<T=unknown>(key: KeyType<T>, ...args: any[]): T;1
+        public resolve<T=unknown>(key: KeyType<T>, ...args: any[]): T;
         private _getRegistryItem<T=unknown>(key: KeyType<T>): ValueType<T>;
         private _getFactoryInstance<T=unknown>(key: KeyType<T>): T;
         private _resolve<T=unknown>(key: KeyType<T>, ...args: any[]);
